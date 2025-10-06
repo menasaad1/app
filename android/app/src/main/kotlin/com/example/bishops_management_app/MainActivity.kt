93f4d78be4214d480184c71a39ebae3fb2225816
menasaad1 <menasaad009@gmail.com>
2025-10-06 19:49:37 +0300

    hj

--- android/app/src/main/kotlin/com/example/bishops_management_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/bishops_management_app/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.bishops_management_app
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
 
